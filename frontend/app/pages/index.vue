2ff4ad4057a627d7f8b8f30e0d8f41ef16d7bc3f
b4its <exynosfam@gmail.com>
2026-04-06 11:36:07 +0800

    feat: add tailwind in nuxt frontend

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-green-800">
+      Tailwind successfully integrated with Nuxt 4
+    </h1>
+  </div>
+</template>
